41cc8dc06d455adb293cf83d28e8f49ee3618dbb
Simon Levesque <simon@foilen.com>
2024-09-18 12:52:37 -0400

    Copy SSH configuration files and set permissions

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 An image with openssh server running and rsync available.
 
 Good to use to backup files from a remote server.
+
+It copies the files in `/config` to `/root/.ssh` and set the permissions on it.
+
+# To test
+
+```
+./create-local-release.sh
+
+rm -rf _test
+mkdir -p _test
+
+# Set authorized_keys
+cat > _test/authorized_keys << EOF
+   ssh-rsa AAAAB[...]
+EOF
+
+# Start
+docker run -ti --rm -v $(pwd)/_test:/config -p 2222:22 fdi-openssh:main-SNAPSHOT
+
+# Test ssh
+ssh -o StrictHostKeyChecking=no -p 2222 root@localhost
+
+```
--- assets/init.sh
+++ assets/init.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 set -e
 
+# Copy the files from /config if present
+if [ -d /config ]; then
+  echo "Copying /config to /root/.ssh"
+  cp -vr /config/* /root/.ssh
+
+  echo "Fixing .ssh permissions"
+  chmod 700 /root/.ssh
+
+  if [ -f /root/.ssh/authorized_keys ]; then
+    echo "Fixing authorized_keys permissions"
+    chmod 644 /root/.ssh/authorized_keys
+  fi
+  if [ -f /root/.ssh/id_rsa ]; then
+    echo "Fixing id_rsa permissions"
+    chmod 600 /root/.ssh/id_rsa
+  fi
+  if [ -f /root/.ssh/known_hosts ]; then
+    echo "Fixing known_hosts permissions"
+    chmod 644 /root/.ssh/known_hosts
+  fi
+fi
+
 # Start openssh server
 echo "Starting sshd"
 service ssh start
 
 # Get its pid
+sleep 2s
 pid=$(cat /var/run/sshd.pid)
 echo "sshd started with pid $pid"
 
